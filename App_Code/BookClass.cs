36b15047cbc3a19356434ca629bca2470cd39797
atkozie <atkozie@gmail.com>
2017-03-13 11:36:10 -0700

    Initial Commit

---
+++ App_Code/BookClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data; //these libraries must be added
+using System.Data.SqlClient; //sql server tools
+using System.Configuration; //to talk to the config file
+
+/// <summary>
+/// Summary description for GrantRequestClass
+/// </summary>
+public class BookClass
+{
+    //declare the connection string
+    private SqlConnection connect;
+    public BookClass()
+    {
+        //get the connection string
+        string connectString =
+            ConfigurationManager.ConnectionStrings["BookReviewConnection"].ToString();
+        //initilize the connection object
+        connect = new SqlConnection(connectString);
+    }
+
+    //this method takes a parameter authorkey
+    //and returns all the books for that author
+    public DataTable Getbooks(int authorKey)
+    {
+        //declare a new data table to store the results
+        DataTable table = new DataTable();
+        //sql statement
+        string sql = "Select * From Book inner Join AuthorBook " +
+            "on Book.BookKey=AuthorBook.BookKey " +
+            "Where AuthorKey = @author";
+        //the command object passes the sql through the connection
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        //the parameter replaces the sql variable with a value
+        cmd.Parameters.AddWithValue("@author", authorKey);
+        //declare a reader to stream the results
+        SqlDataReader reader = null;
+        //open the connection
+        connect.Open();
+        //execute the command and reader
+        reader = cmd.ExecuteReader();
+        //load the results into the table 
+        table.Load(reader);
+        //close stuff
+        reader.Close();
+        connect.Close();
+        return table;
+    }
+
+    public DataTable GetAuthors()
+    {
+        DataTable table = new DataTable();
+        string sql = "Select AuthorKey, AuthorName from Author";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        SqlDataReader reader = null;
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        table.Load(reader);
+        reader.Close();
+        connect.Close();
+        return table;
+    }
+}
---
+++ GrantRequests.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class GrantRequests : System.Web.UI.Page
+{
+    BookClass bc = new BookClass();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //make sure the session is active
+        if (Session["userKey"] != null)
+        {
+            //if good and if not a postback populate
+            //drop down list
+            if (!IsPostBack)
+            {
+                DataTable table = bc.GetAuthors();
+                DropDownList1.DataSource = table;
+                //text displays--value is stored
+                DropDownList1.DataTextField = "AuthorName";
+                DropDownList1.DataValueField = "AuthorKey";
+                DropDownList1.DataBind();
+                ListItem item = new ListItem("Choose an Author");
+                DropDownList1.Items.Insert(0, item);
+            }
+        }
+        else
+        {
+            Response.Redirect("Default.aspx");
+        }
+    }
+
+    protected void FillGrid()
+    {
+        if (!DropDownList1.SelectedItem.Text.Equals("Choose an Author"))
+        {
+            //get the key for the selected author from the values 
+            //in the drop down list
+            int key = int.Parse(DropDownList1.SelectedValue.ToString());
+            //pass it to the class method and get the datatable
+            //of results
+            DataTable tbl = bc.Getbooks(key);
+            //bind it to the Gridview
+            GridView1.DataSource = tbl;
+            GridView1.DataBind();
+        }
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        FillGrid();
+    }
+}
